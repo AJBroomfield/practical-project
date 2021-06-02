82357b22eebf717675f28eaf146788e9b34971ac
Andrew <ajbroomfield94@gmail.com>
2021-06-02 15:41:41 +0000

    add jenkins to the docker group - installdocker.sh

--- scripts/installdocker.sh
+++ scripts/installdocker.sh
@@ -15,6 +15,7 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $(whoami)
 
 #Docker login
 docker login -u $DOCKER_LOGIN_USR-p $DOCKER_LOGIN_PSW
