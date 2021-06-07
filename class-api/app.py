ece5c140c42e4636049e273d357152e4c16840a4
Andrew Broomfield <82602104+AJBroomfield@users.noreply.github.com>
2021-06-07 10:14:13 +0100

    Update app.py

--- class-api/app.py
+++ class-api/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/get_class', methods=['GET'])
 def get_class():
-    return random.choice(['Barbarian','Bard','Cleric','Driud','Fighter','Monk','Paladin','Ranger','Rogue','Sorcerer','Warlock','Wizard'])
+    #return random.choice(['Barbarian','Bard','Cleric','Driud','Fighter','Monk','Paladin','Ranger','Rogue','Sorcerer','Warlock','Wizard'])
+    return random.choice(['Cleric'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
